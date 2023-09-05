7eab35d9839657854e7c68c3625e11afd15c789b
Jacob Brown <brown.jacob.alexander@gmail.com>
2023-09-05 08:48:20 -0600

    Noting some lesson takeaways

--- class-demos/flask_hello_app.py
+++ class-demos/flask_hello_app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://brown:{PASSWORD}@localhost:5432/example'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# instantiate before creating the class?
 db = SQLAlchemy(app)
 
 class Person(db.Model):
