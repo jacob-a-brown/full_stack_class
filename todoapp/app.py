b8b4620e6e445148c723d8c2c7e5c5b55837a313
Jacob Brown <brown.jacob.alexander@gmail.com>
2023-09-07 16:20:51 -0600

    Created checkboxes and working on delete button

--- todoapp/app.py
+++ todoapp/app.py
@@ -23,7 +23,7 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-# route to listen to todos/create. requests that come in with the method "post"
+# handler to listen to todos/create. requests that come in with the method "post"
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
@@ -38,6 +38,7 @@ def create_todo():
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
+        body['id'] = todo.id
     except:
         error=True
         db.session.rollback()
@@ -50,10 +51,37 @@ def create_todo():
     else:
         return jsonify(body)
 
-# listens to home page        
+@app.route('/todos/<todo_id>/set-completed', methods=["POST"])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exec_info())
+    finally:
+        db.session.close()
+
+    return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>/delete-me', methods=['DELETE'])
+def delete_me(todo_id):
+    try:
+        pass
+    except:
+        pass
+    finally:
+        pass
+
+    return redirect(url_for('index'))
+
+# handler listens to home page        
 @app.route('/')
 def index():
-    return render_template('index.html', data = Todo.query.all())
+    return render_template('index.html', data = Todo.query.order_by('id').all())
 
 if __name__ == '__main__':
     app.debug = True
