f075ad08331e6ccedf0ccd327969c2ffac74e219
Jacob Brown <brown.jacob.alexander@gmail.com>
2023-09-20 08:44:37 -0600

    Work on flask intro

--- flask_setup_example/flaskr/__init__.py
+++ flask_setup_example/flaskr/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask, jsonify
+from pathlib import Path
 
 def create_app(test_config=None):
-    app = Flask(__name__)
+    # instance_relative_config=True tells the app that configuration files
+    # are in the same directory
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapp(
+        # in general SECRET_KEY should *actually* be secret
+        SECRET_KEY='dev',
+        DATABASE=Path(app.instance_path, 'flaskr.sqlite'))
+
+    if test_config is None:
+        #load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
 
     @app.route('/')
     def hello():
